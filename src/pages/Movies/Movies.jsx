da05989ad5be9d3a84c72c1ddfa9d99a7cf19437
NickKononenko <kononenko0812@gmail.com>
2024-02-14 19:23:23 +0200

    updated

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchMoviesBySearchQuery } from 'api/data';
 import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
   const [searchData, setSearchData] = useState('');
   const [data, setData] = useState([]);
-  const [isSubmit, setIsSubmit] = useState(false);
-  const [isFetch, setIsFetch] = useState(true);
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const movieQuery = searchParams.get('q') ?? '';
@@ -19,29 +16,25 @@ const Movies = () => {
   };
 
   useEffect(() => {
-    if (!isFetch) return;
-    setIsFetch(false);
     fetchMoviesBySearchQuery(movieQuery).then(response => {
       setData(response);
     });
-  }, [movieQuery, isFetch]);
+  }, [movieQuery]);
 
   const handleInputChange = e => {
     setSearchData(e.target.value);
-    setIsSubmit(false);
     const nextParams = e.target.value !== '' ? { q: e.target.value } : {};
     setSearchParams(nextParams);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    setIsSubmit(true);
     updateData();
   };
 
   return (
     <>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label>
           <input
             type="text"
@@ -51,13 +44,11 @@ const Movies = () => {
             required
           />
         </label>
-        <button type="submit" onClick={handleSubmit}>
-          Search
-        </button>
+        <button type="submit">Search</button>
       </form>
       <div>
         <ul>
-          {data.results?.length === 0 && isSubmit ? (
+          {data.results?.length === 0 ? (
             <p>{`There is no movies`}</p>
           ) : (
             data.results?.map(movie => {
